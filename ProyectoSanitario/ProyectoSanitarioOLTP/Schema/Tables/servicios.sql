4c2807b2d9d6f7cbb10e0bb7e415a4bfb0166ddf
Benjamin-Tejerina <btejerinal@soe.uagrm.edu.bo>
2025-02-26 19:12:21 -0400

    Se subieron 2 tablas medicos y servicios

---
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/servicios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [san].[Servicios] 
+(
+    [idServicio] NVARCHAR(10) , 
+    [NombreCompleto] NVARCHAR(100) NOT NULL,
+    [Comentario] NVARCHAR(255),
+    CONSTRAINT [PK_pidServicio] PRIMARY KEY ([idServicio])
+  );
