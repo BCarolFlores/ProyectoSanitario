22a0eb4fb0d4c767158200a2a840b5a871da4c09
Benjamin-Tejerina <btejerinal@soe.uagrm.edu.bo>
2025-02-26 19:54:02 -0400

    Se ajustaron las tablas medicos y servicios

--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/medicos.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/medicos.sql
@@ -1,8 +1,10 @@
 CREATE TABLE [san].[Medicos] (
+    [IdMedico] Int ,
     [DNI] NVARCHAR(10) ,
     [ApellidosNombre] NVARCHAR(100) NOT NULL,
     [FechaNacimiento] DATE,
     [codHospital] INT,
     [EsDirector] BIT DEFAULT 0, -- 1 si es director, 0 si no
+    CONSTRAINT [PK_IdMedico] PRIMARY KEY ([IdMedico]),
     FOREIGN KEY (codHospital) REFERENCES Hospitales(codHospital)
   );
