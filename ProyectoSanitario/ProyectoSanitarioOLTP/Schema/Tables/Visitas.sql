d10cc0ca6cac32f40df4a65e64d2691bb2525acb
mycalderonc <mycalderonc@soe.uagrm.edu.bo>
2025-02-26 20:12:08 -0400

    Tabla Visitas

---
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/Visitas.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE Visitas (
+    VisitaID INT PRIMARY KEY IDENTITY(1, 1),
+    codHist INT,
+    FechaHora DATETIME NOT NULL,
+    codHospital INT,
+    idServicio NVARCHAR(10),
+    DNI NVARCHAR(10), -- Médico que atendió
+    Diagnostico NVARCHAR(255),
+    Tratamiento NVARCHAR(255),
+    Ingreso BIT DEFAULT 0, -- 1 si hubo ingreso, 0 si no
+    NumeroHabitacion NVARCHAR(10),
+    FechaAlta DATE,
+    FcodHist int, 
+    FcodHospital int,
+    FidServicio int, 
+    FDNI int
+   -- FOREIGN KEY (codHist) REFERENCES HistoriasClinicas(codHist),
+   -- FOREIGN KEY (codHospital) REFERENCES Hospitales(codHospital),
+   -- FOREIGN KEY (idServicio) REFERENCES Servicios(idServicio),
+   -- FOREIGN KEY (DNI) REFERENCES Medicos(DNI)
+);
