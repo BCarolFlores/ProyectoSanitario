e9eb5d8547800a2897650cfd55f146d5df2dbe17
mycalderonc <mycalderonc@soe.uagrm.edu.bo>
2025-02-26 20:48:55 -0400

    confirmacion

--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/Visitas.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/Visitas.sql
@@ -10,12 +10,11 @@
     Ingreso BIT DEFAULT 0, -- 1 si hubo ingreso, 0 si no
     NumeroHabitacion NVARCHAR(10),
     FechaAlta DATE,
-    FcodHist int, 
-    FcodHospital int,
-    FidServicio int, 
-    FDNI int
-   -- FOREIGN KEY (codHist) REFERENCES HistoriasClinicas(codHist),
-   -- FOREIGN KEY (codHospital) REFERENCES Hospitales(codHospital),
-   -- FOREIGN KEY (idServicio) REFERENCES Servicios(idServicio),
-   -- FOREIGN KEY (DNI) REFERENCES Medicos(DNI)
+ 
+   FOREIGN KEY (codHist) REFERENCES HistoriasClinicas(codHist),
+   FOREIGN KEY (codHospital) REFERENCES Hospitales(codHospital),
+   FOREIGN KEY (idServicio) REFERENCES Servicios(idServicio),
+   FOREIGN KEY (DNI) REFERENCES Medicos(DNI)
 );
+
+
