aee13dad7e7fd9d5c59fb8379691483a59c90647
DavidGuzmanZ <ndguzmanz@soe.uagrm.edu.bo>
2025-02-26 19:05:52 -0800

    Tablas Hospital y Paciente

--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/hospital.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/hospital.sql
@@ -1,8 +1,12 @@
-﻿CREATE TABLE [san].[hospital]
+﻿CREATE TABLE [dbo].[Hospital](
+	[idHospital] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nvarchar](100) NOT NULL,
+	[Ciudad] [nvarchar](100) NOT NULL,
+	[Telefono] [nvarchar](15) NULL,
+	[DirectorDNI] [nvarchar](10) NULL,
+PRIMARY KEY CLUSTERED 
 (
-	[cod_hospital] INT NOT NULL IDENTITY(1, 1), 
-    [nombre] NVARCHAR(50) NULL, 
-    [ciudad] NVARCHAR(50) NULL, 
-    [telefono] NVARCHAR(50) NULL, 
-    CONSTRAINT [PK_hospital_cod] PRIMARY KEY ([cod_hospital]) 
-)
+	[idHospital] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/paciente.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/paciente.sql
@@ -1,9 +1,13 @@
-﻿CREATE TABLE san.paciente ( --probando un cambio
-	idPaciente INT  IDENTITY(1, 1) NOT NULL,
-    DNI NVARCHAR(10) NULL,
-    ApellidosNombre NVARCHAR(100) NULL,
-    FechaNacimiento DATE NULL,
-    NumeroSeguridadSocial NVARCHAR(20) NULL,
-    OtrosDatos NVARCHAR(255)
-    CONSTRAINT PK_idPaciente PRIMARY KEY (idPaciente) NULL
-);
+﻿CREATE TABLE [dbo].[Paciente](
+	[idPaciente] [int] IDENTITY(1,1) NOT NULL,
+	[DNI] [nvarchar](10) NULL,
+	[ApellidosNombre] [nvarchar](100) NOT NULL,
+	[FechaNacimiento] [date] NULL,
+	[NumeroSeguridadSocial] [nvarchar](20) NOT NULL,
+	[OtrosDatos] [nvarchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idPaciente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
