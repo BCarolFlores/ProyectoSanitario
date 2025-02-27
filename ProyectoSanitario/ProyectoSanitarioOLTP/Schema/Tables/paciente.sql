329ffc1f1288e1530a4ca26dc3922e19315c043f
BCarolFlores <bcfloresv@soe.uagrm.edu.bo>
2025-02-26 22:33:50 -0400

    Commit Paciente table

--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/paciente.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/paciente.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE san.paciente ( --probando un cambio
 	idPaciente INT  IDENTITY(1, 1) NOT NULL,
-    DNI NVARCHAR(10) NULL,
-    ApellidosNombre NVARCHAR(100) NULL,
-    FechaNacimiento DATE NULL,
-    NumeroSeguridadSocial NVARCHAR(20) NULL,
-    OtrosDatos NVARCHAR(255)
+    DNI NVARCHAR(10) NOT NULL,
+    ApellidosNombre NVARCHAR(100) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    NumeroSeguridadSocial NVARCHAR(20) NOT NULL,
+    OtrosDatos NVARCHAR (255) NULL
     CONSTRAINT PK_idPaciente PRIMARY KEY (idPaciente) NULL
 );
