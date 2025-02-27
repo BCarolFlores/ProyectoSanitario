099ec6d03e846bf0c3b25ead702a715f143879d0
DavidGuzmanZ <ndguzmanz@soe.uagrm.edu.bo>
2025-02-26 19:25:15 -0800

    Update HistoriaClinica.sql
    agregamos foreingkey

--- ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/HistoriaClinica.sql
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/HistoriaClinica.sql
@@ -2,9 +2,13 @@ CREATE TABLE [san].[HistoriasClinica](
 	[idHist] [int] IDENTITY(1,1) NOT NULL,
 	[codHist] [int] NOT NULL,
 	[idPaciente] [int] NULL,
+	FOREIGN KEY([idPaciente]) REFERENCES [san].[Paciente] ([idPaciente]),
 PRIMARY KEY CLUSTERED 
 (
 	[idHist] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
+GO
+
 GO
