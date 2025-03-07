b53c4400b1f618a8e51d41e424b409a96fb2a89e
Alex Flores Lopez <afloresl@soe.uagrm.edu.bo>
2025-03-07 19:13:16 -0400

    fixed dimHospital

--- ProyectoSanitario/ProyectSanitarioDW/Schema/Tables/DimHospital.sql
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Tables/DimHospital.sql
@@ -4,11 +4,7 @@
     codHospital NVARCHAR(20),
     Nombre NVARCHAR(100),
     Ciudad NVARCHAR(100),
-<<<<<<< HEAD
-    Telefono NVARCHAR(20),
-=======
-    Telefono INT, --NVARCHAR(15),
->>>>>>> 9f86785b70182bacc70f035a5e4b1cb273b6cf6d
+    Telefono NVARCHAR(15),
     DirectorDNI NVARCHAR(10),
     RowVersion TIMESTAMP
 );
