a4359cae9d855a24e64a991f026ce848308e3d4c
Benjamin-Tejerina <btejerinal@soe.uagrm.edu.bo>
2025-03-08 12:49:47 -0400

    Se crearon las tablas Medico y Servicio en el esquema Staging.

---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Tables/staging/Staging.Medico.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE staging.Medico (
+    idMedicoSK INT NOT NULL,
+    DNI NVARCHAR(12),
+    Nombre NVARCHAR(100),
+    Apellidos NVARCHAR(110),
+    FechaNacimiento DATE,
+    EsDirector BIT
+);
---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Tables/staging/Staging.Servicio.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE staging.Servicio (
+    idServicioSK INT NOT NULL,
+    NombreCompleto NVARCHAR(100),
+    Comentario NVARCHAR(255)
+);
