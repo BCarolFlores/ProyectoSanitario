ead0ae4918b825a4a5b45121352444523f50c520
Benjamin-Tejerina <btejerinal@soe.uagrm.edu.bo>
2025-03-08 13:28:30 -0400

    Se creo la tabla PackageConfig, y se crearon los procedimientos (GetLastPackageRowVersion, UpdateLastPackageRowVersion﻿)

---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Stored Procedures/GetLastPackageRowVersion.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE san.GetLastPackageRowVersion
+
+	@tableName NVARCHAR(100)
+
+  AS
+  BEGIN
+	SELECT LastRowVersion
+	FROM san.PackageConfig
+	WHERE TableName = @tableName;
+  END
+GO
---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Stored Procedures/UpdateLastPackageRowVersion.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE san.UpdateLastPackageRowVersion﻿
+	@tableName NVARCHAR(100)
+	,@lastRowVersion BIGINT
+  
+  AS
+  BEGIN
+	UPDATE san.PackageConfig
+	SET LastRowVersion = @lastRowVersion
+	WHERE TableName = @tableName;
+  END
+GO
