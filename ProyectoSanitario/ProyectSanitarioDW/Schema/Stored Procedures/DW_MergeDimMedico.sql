ba8afddebc4b669f9dfac515814b476cceef8bab
Benjamin-Tejerina <btejerinal@soe.uagrm.edu.bo>
2025-03-08 13:56:31 -0400

    Se adicionaron el script para los procedimientos almacenados (DW_MergeDimMedico y DW_MergeDimServicio)

---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Stored Procedures/DW_MergeDimMedico.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE san.DW_MergeDimMedico
+AS
+BEGIN
+    UPDATE dc
+    SET [DNI] = sc.[DNI],
+        [Nombre] = sc.[Nombre],
+        [Apellidos] = sc.[Apellidos],
+        [FechaNacimiento] = sc.[FechaNacimiento],
+        [EsDirector] = sc.[EsDirector]
+    FROM san.DimMedico dc
+    INNER JOIN staging.Medico sc ON dc.[idMedicoSK] = sc.[idMedicoSK];
+END
+GO
---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Stored Procedures/DW_MergeDimServicio.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE san.DW_MergeDimServicio
+AS
+BEGIN
+    UPDATE dc
+    SET [NombreCompleto] = sc.[NombreCompleto],
+        [Comentario] = sc.[Comentario]
+    FROM san.DimServicio dc
+    INNER JOIN staging.Servicio sc ON dc.[idServicioSK] = sc.[idServicioSK];
+END
+GO
