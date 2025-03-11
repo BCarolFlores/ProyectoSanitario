91b472c1b96e831e84636a01e3bc17914f62ec15
DavidGuzmanZ <ndguzmanz@soe.uagrm.edu.bo>
2025-03-11 14:04:41 -0400

    Actualizamos para que todo ejecute. Add Script para llenar dim tiempo,y packgageConfig

---
+++ ProyectoSanitario/ProyectSanitarioDW/Schema/Stored Procedures/DW_PopulateDimTiempo.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE PopulateDimTiempo
+AS
+BEGIN
+    DECLARE @StartDate DATE = '2016-01-01'
+    DECLARE @EndDate DATE = DATEADD(YEAR, 10, @StartDate)
+
+    WHILE @StartDate <= @EndDate
+    BEGIN
+        INSERT INTO san.DimTiempo (Fecha, Anio, Mes, Dia, DiaSemana, Trimestre)
+        VALUES (
+            @StartDate,
+            YEAR(@StartDate),
+            MONTH(@StartDate),
+            DAY(@StartDate),
+            DATEPART(WEEKDAY, @StartDate),
+            DATEPART(QUARTER, @StartDate)
+        );
+        SET @StartDate = DATEADD(DAY, 1, @StartDate);
+    END
+END
